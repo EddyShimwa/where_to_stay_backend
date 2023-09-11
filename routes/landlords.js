a6deef3475ae47f100e8e310c7836bd6ad24b2a8
EddyShimwa <shedpray16@gmail.com>
2023-09-11 21:31:51 +0200

    modify landlords routes

--- routes/landlords.js
+++ routes/landlords.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const landlordController = require('../controllers/landlordsContoller');
 
-// Route to create a new landlord accountt
-router.post('/create', landlordController.createLandlord);
+router.post('/applications/create', landlordController.createLandlord);
 
-// Route to retrieve all landlords
 router.get('/landlords', landlordController.getAllLandlords) ;
 
-// Route to retrieve landlord details by ID
 router.get('/landlords/:id', landlordController.getLandlordById);
 
-// Route to update landlord information by ID
-router.put('landlords/:id', landlordController.updateLandlordById);
+// router.put('landlords/:id', landlordController.updateLandlordById);
 
 module.exports = router;
