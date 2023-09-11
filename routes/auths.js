8f5657765ab78cbf52b83c3131c85049faf0fecc
EddyShimwa <shedpray16@gmail.com>
2023-09-11 21:39:53 +0200

    add the routes auths

---
+++ routes/auths.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const authsController = require('../controllers/authsController');
+
+
+router.post('/auths/signup', authsController.registerUser);
+router.post('/auths/login', authsController.login);
+
+module.exports = router;
