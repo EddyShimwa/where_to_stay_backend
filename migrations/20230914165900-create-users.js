b53c3ed700810c8521e4807e263abbf66c42b83c
EddyShimwa <shedpray16@gmail.com>
2023-09-14 23:19:33 +0200

    add migrations

---
+++ migrations/20230914165900-create-users.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+      await queryInterface.dropTable('users');
+
+  }
+};
