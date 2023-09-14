f6b74679c5487efb145665cf05088ad4860cf2d1
EddyShimwa <shedpray16@gmail.com>
2023-09-14 23:18:49 +0200

    modify auths controller

--- controllers/authsController.js
+++ controllers/authsController.js
@@ -1,217 +1,96 @@
 const bcrypt = require('bcryptjs');
 const dotenv = require('dotenv');
 const db = require('../models');
+const jwt = require('jsonwebtoken');
 dotenv.config();
 
+const User = db.User;
+const Property = db.Property;
+const Application = db.Application; 
+
+// const User = db.User;
 const provideToken = (id, email) => {
   const token = jwt.sign({ id, email }, process.env.JWT_SECRET, { expiresIn: '1h' });
   return token;
-}
+};
 
-//sign up
-const registerStudent = async (req, res) => {
+
+// Sign up
+const registerUser = async (req, res) => {
   try {
     const {
       firstName,
       lastName,
       email,
       password,
+      role,
     } = req.body;
 
-    // Check if a student with the given email exists
-    const existingStudent = await db.Student.findOne({
+    // Check if a User with the given email exists
+
+    const existingUser = await User.findOne({
       where: { email },
     });
 
-    if (existingStudent) {
+    if (existingUser) {
       return res.status(409).json({ error: 'Email already exists' });
     }
 
     const hashedPassword = bcrypt.hashSync(password.trim(), Number(process.env.SALT_ROUNDS));
-    const student = await db.Student.create({
+    const user = await User.create({
       firstName: firstName.toLowerCase(),
       lastName: lastName.toLowerCase(),
       email,
       password: hashedPassword,
+      role: role.toLowerCase(),
     });
-const studentResponse = {
-  firstName: student.firstName,
-  lastName: student.lastName,
-  email: student.email,
-}
-    const token = provideToken(student.id, email);
-    return res.status(201).json({ message: 'student registered successfully', studentResponse, token });
+
+    const userResponse = {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+    };
+
+    const token = provideToken(user.id, email);
+
+    return res.status(201).json({ message: 'Student registered successfully', userResponse, token });
   } catch (error) {
+    console.log(error)
     return res.status(500).json({ error: 'Internal Server Error detected' });
   }
 };
 
+// Login route
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const student = await db.Student.findOne({
+    const user = await User.findOne({
       where: {
         email,
       },
-      attributes: ['id', 'email', 'password'],
+      attributes: ['id', 'email', 'password', 'role'],
     });
 
-    if (!student) {
+    if (!user) {
       return res.status(401).json({ error: 'Incorrect email or password' });
     }
-
-    if (bcrypt.compareSync(password, student.password)) {
-      const { id } = student.dataValues;
+    // check if password is correct
+    if (bcrypt.compareSync(password, user.password)) {
+      const { id, role } = user.dataValues;
       const token = provideToken(id, email);
+      return res.status(200).json({ message: `${role} successfuly logged in`, token });
       // localStorage.setItem('token', token);
-      return res.status(200).json({ message: 'student successfully logged in' });
+      
     }
 
-    return res.status(401).json({ error: 'Error logging in' });
+    return res.status(401).json({ error: 'incorrect email or password' });
   } catch (error) {
-    return res.status(500).json({ error: 'Internal Server Error detected' });
+    return res.status(500).json({ error: 'something went wrong' });
   }
 };
 
 module.exports = {
-  registerStudent,
+  registerUser,
   login,
 };
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   const bcrypt = require('bcryptjs');
-// const dotenv = require('dotenv');
-// const db = require('../models');
-// const provideToken = require('../utils/token');
-
-// dotenv.config();
-
-// const Landlord = db.landlords;
-// const Student = db.Student;
-
-// // Authenticate a student
-// exports.authenticateStudent = async (req, res) => {
-//   try {
-//     const { student_email, student_password } = req.body;
-
-//     // Find the student by email
-//     const student = await Student.findOne({
-//       where: {
-//         student_email,
-//       },
-//       attributes: ['id', 'student_email', 'student_password'],
-//     });
-
-//     if (!student) {
-//       return res.status(401).json({ error: 'Incorrect email or password' })
-//     }
-
-//     // Compare the password
-//     if (bcrypt.compareSync(student_password, student.student_password)) {
-//       const { id } = student.dataValues;
-//       const token = provideToken(id, student_email, 'student');
-//       return res.status(200).json({ message: 'Student authenticated successfully', student, token });
-//     }
-
-//     return res.status(401).json({ error: 'Incorrect email or password' });
-
-//   } catch (error) {
-//     console.error("Database error:", error);
-//     return res.status(500).json({ error: 'Internal Server Error detected' });
-//   }
-// }
-
-// // Authenticate a landlord
-// exports.authenticateLandlord = async (req, res) => {
-//   try {
-//     const { landlord_email, landlord_password } = req.body;
-
-//     // Find the landlord by email
-//     const landlord = await Landlord.findOne({
-//       where: {
-//         landlord_email,
-//       },
-//       attributes: ['id', 'landlord_email', 'landlord_password'],
-//     });
-
-//     if (!landlord) {
-//       return res.status(401).json({ error: 'Incorrect email or password' })
-//     }
-
-//     // Compare the password
-//     if (bcrypt.compareSync(landlord_password, landlord.landlord_password)) {
-//       const { id } = landlord.dataValues;
-//       const token = provideToken(id, landlord_email, 'landlord');
-//       return res.status(200).json({ message: 'Landlord authenticated successfully', landlord, token });
-//     }
-
-//     return res.status(401).json({ error: 'Incorrect email or password' });
-
-//   } catch (error) {
-//     console.error("Database error:", error);
-//     return res.status(500).json({ error: 'Internal Server Error detected' });
-//   }
-// }
