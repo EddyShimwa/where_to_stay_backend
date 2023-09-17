0451cfda8683a335d71e3db5b31f5d99a4a9e147
EddyShimwa <shedpray16@gmail.com>
2023-09-18 01:21:25 +0200

    adjust the config

--- config/config.js
+++ config/config.js
@@ -1,14 +1,20 @@
-// config/config.js
+require('dotenv').config();
 
 module.exports = {
     production: {
+      url: 'DATABASE_URL',
       dialect: 'postgres', 
         ssl: {
           require: true,
           rejectUnauthorized: false 
       },
-      url: process.env.DATABASE_URL,
     },
-
+    development: {
+        dialect: 'postgres',
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+    },
   };
   
