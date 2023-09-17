0584e9c8c99f091e92ffda12360fdeb520c907b0
EddyShimwa <shedpray16@gmail.com>
2023-09-17 21:44:43 +0200

    modify config file

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 module.exports = {
     production: {
-        username: eddy,
+        username: 'eddy',
         port: 5432,
         password: '1Gs9CXEwJo3JIsM0I6PrnPJvlkGJDwAA',
-        database: wheretostay,
-        host: dpg-ck23i3o21fec73b49nkg-a,
+        database: 'wheretostay',
+        host: 'dpg-ck23i3o21fec73b49nkg-a',
         dialect: 'postgres',
         logging: false,
         dialectOptions: {
@@ -19,7 +19,7 @@ module.exports = {
         },
       },
     development: {
-        username: process.env.DB_USER,
+        username: process.env.DB_USER  ,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
