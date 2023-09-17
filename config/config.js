07188059df415644223e9972afb6ff53d59d8b7f
EddyShimwa <shedpray16@gmail.com>
2023-09-17 23:16:21 +0200

    add dialect options

--- config/config.js
+++ config/config.js
@@ -3,8 +3,12 @@ module.exports = {
         use_env_variable: 'DATABASE_URL', 
         dialect: 'postgres',
         logging: false,
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false
+            }
+        
     },
+}
 };
-
-
-
