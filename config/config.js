265745f1ad3a5577632c8cd431d7eed87d893b38
EddyShimwa <shedpray16@gmail.com>
2023-09-18 01:45:26 +0200

    changes

--- config/config.js
+++ config/config.js
@@ -9,16 +9,9 @@ module.exports = {
           rejectUnauthorized: false 
       },
     },
-    // development: {
-    //    dialect: 'postgres',
-    //    DATABASE_URL: "DATABASE_URL",
-    //     dialect: 'postgres', 
-    //       ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false 
-    //     },
+    development: {
+       dialect: 'postgres',
+    },
 
 
-    // },
-  };
-  
+}
