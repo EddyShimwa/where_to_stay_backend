5f2e4e00587580fc2df4cc619c691f433f0b232c
EddyShimwa <shedpray16@gmail.com>
2023-09-18 00:18:09 +0200

    add the node version

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
     production: {
-        use_env_variable: 'DATABASE_URL', 
-        dialect: 'postgres',
-        logging: false,
-        dialectOptions: {
-            ssl: {
-              require: true,
-              rejectUnauthorized: false
-            }
-        
-    },
-}
-};
+      use_env_variable: 'DATABASE_URL',
+      dialect: 'postgres', // Specify the dialect (in this case, PostgreSQL)
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      }
+    }
+  };
+  
