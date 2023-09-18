f74bb12e952c879fabb144ccfb4cbf76b6d0a1f3
EddyShimwa <shedpray16@gmail.com>
2023-09-18 12:19:03 +0200

    fix cors errors

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.use(cors({
+    origin: 'http://localhost:3001', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  }));
+
+
 //Routes
 const studentRoutes = require('./routes/students');
 const landlordRoutes = require('./routes/landlords');
 const propertyRoutes = require('./routes/properties');
-// const applicationRoutes = require('./routes/applications');
 const authsRouter = require('./routes/auths');
 
 // using routes
