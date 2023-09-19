c1b9b47c9e57164ff289f035474111323cfd8884
EddyShimwa <shedpray16@gmail.com>
2023-09-19 12:08:55 +0200

    add associations on booking model

--- models/booking.js
+++ models/booking.js
@@ -1,37 +1,41 @@
-
+// models/booking.js
 module.exports = (sequelize, DataTypes) => {
     const Booking = sequelize.define('Booking', {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        student_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        property_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      student_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      property_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     }, {
-        tableName: 'bookings',
-        timestamps: true,
+      tableName: 'bookings',
+      timestamps: true,
     });
-
+  
     Booking.associate = (models) => {
-        Booking.belongsTo(models.User, { 
-            foreignKey: 'userId', 
-            onDelete: 'CASCADE',
-        });
-        Booking.belongsTo(models.Property, {
-            foreignKey: 'propertyId', 
-            onDelete: 'CASCADE',
-        });
+      Booking.belongsTo(models.User, {
+        foreignKey: 'student_id',
+        targetKey: 'id',
+        onDelete: 'CASCADE',
+        constraints: false, 
+        scope: {
+          role: 'student', 
+        },
+      });
+      Booking.belongsTo(models.Property, {
+        foreignKey: 'property_id',
+        onDelete: 'CASCADE',
+        as: 'property',
+      });
     };
-
+  
     return Booking;
-};
-
-
-
+  };
+  
