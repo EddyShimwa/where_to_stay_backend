385663b81b6ad50a22e5861bf201e2b7e76786cc
EddyShimwa <shedpray16@gmail.com>
2023-09-23 17:14:04 +0200

    modify the bookings controller

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -44,8 +44,7 @@ const createBooking = async (req, res) => {
     const landlordUserId = property.userId;
     const notificationContent = `New booking for your property: ${property.description}. Contact student at ${user.email}.`;
 
-    // Use your notification system to send the notification to the landlord
-    // sendNotification(landlordUserId, notificationContent);
+   io.to(landlordUserId).emit('newBooking', notificationContent);
 
     res.status(201).json(newBooking);
   } catch (error) {
@@ -53,12 +52,30 @@ const createBooking = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
-// //delete booking
-// const deleteBooking = async (req, res) => {
+//cancel booking 
 
-// }
+const cancelBooking = async (req, res) => {
+  const bookingId = req.params.id;
+  try {
+    const booking = await Booking.findOne({
+      where: {
+        id: bookingId,
+      },
+    });
+    if (!booking) {
+      res.status(404).json({ error: 'Booking not found' });
+    } else {
+      await booking.destroy();
+      res.status(200).json({ message: 'You have successfully cancelled this booking' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 
 module.exports = {
   createBooking,
+  cancelBooking
 };
