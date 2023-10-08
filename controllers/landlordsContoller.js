1f03affee633b91a8b8d02c602288068949b49cd
EddyShimwa <shedpray16@gmail.com>
2023-10-08 16:07:46 +0200

    fetch the booking id

--- controllers/landlordsContoller.js
+++ controllers/landlordsContoller.js
@@ -23,7 +23,7 @@ const getAllLandlords = async (req, res) => {
 }
 
 const getStudentsBooked = async (req, res) => {
-   const propertyId = req.params.id;
+  const propertyId = req.params.id;
   try {
     const property = await Property.findOne({
       where: {
@@ -36,11 +36,11 @@ const getStudentsBooked = async (req, res) => {
       return res.status(404).json({ error: 'Property not found or does not belong to the specified landlord' });
     }
 
-    const students = await Booking.findAll({
+    const bookings = await Booking.findAll({
       where: {
         property_id: propertyId,
       },
-      attributes: ['student_id'], 
+      attributes: ['id'], // Include the booking ID
       include: [
         {
           model: User,
@@ -49,6 +49,12 @@ const getStudentsBooked = async (req, res) => {
         },
       ],
     });
+
+    // Modify the response format to include both student info and booking ID
+    const students = bookings.map(booking => ({
+      bookingId: booking.id,
+      student: booking.student,
+    }));
 
     res.status(200).json(students);
   } catch (error) {
